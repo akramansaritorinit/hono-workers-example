6a5aa35ae9fd08a10c408b2ec49124de3fcf38d9
akramansaritorinit <akram.ansari@torinit.ca>
2023-08-28 15:55:05 +0530

    minor fix

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -7,6 +7,7 @@ export const Layout = (props: { children: HtmlEscapedString | HtmlEscapedString[
         <head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <script src="https://unpkg.com/htmx.org@1.9.3"></script>
+            <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
             <script src="https://cdn.tailwindcss.com"></script>
             <title>Hono App</title>
         </head>
